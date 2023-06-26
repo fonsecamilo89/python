dde90d9ac7940f20a00f0f158f6ec7616b334f3c
julianfonseca01 <julian.fonseca01@uptc.edu.co>
2023-06-26 10:48:17 -0500

    fix names

--- 002 consolaInteractiva.py
+++ 002 consolaInteractiva.py
@@ -1,17 +1,7 @@
-
-# mensaje = "hola soy camilo"
-# print(mensaje)
-# print("su mensaje mide: " + str(len(mensaje)))
-
-
-
-
- # This program says hello and asks for my name.
-
 print('Hello, world!')
 print('What is your name?') # ask for their name
 
-myName = input()
+myName = str(input())
 
 print('It is good to meet you, ' + myName)
 print('The length of your name is: ' + str(len(myName)))
@@ -19,10 +9,11 @@ print('The length of your name is: ' + str(len(myName)))
 
 print('What is your age?') # ask for their age
 
-myAge = input()
+myAge = int(input())
 
+print(myAge)
 print(type(myAge))
 
-print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+print('You will be ' + str(myAge + 1) + ' in a year.')
 # Es necesario realizar casting de myAge con int(myAge) para poder sumarle 1.
 # Posteriormente es necesario convertir la suma a string para poder mostrar el resultado
